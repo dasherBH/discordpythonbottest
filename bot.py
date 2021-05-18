82bd8530f1491b2df71eaded1edfac781fd08759
dasherBH <68877923+dasherBH@users.noreply.github.com>
2021-05-18 22:14:02 +0200

    New Test Changes

--- bot.py
+++ bot.py
@@ -1,13 +1,11 @@
 import discord 
 from discord.ext import commands
 
-token = "" 
-
 bot = commands.Bot(command_prefix='.')
 
-@bot.event()
-aysinc def on_ready():
+@bot.event
+async def on_ready():
   print("Bot Is Ready")
   
   
-bot.run(token)
+bot.run(DISCORD_TOKEN)
